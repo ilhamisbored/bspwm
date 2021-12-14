cb6371043c8272fbab01dc044b2230a94d50fabe
Ilham Zaman <68147233+ItzIlhxm@users.noreply.github.com>
2021-12-14 11:36:43 +0600

    Add files via upload

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,3 +5,6 @@ echo "..." | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar example >> /tmp/polybar1.log 2>&1 &
 
 echo "Polybar is up and running!"
+
+bspc config normal_border_color "#BF616A"
+bspc config focused_border_color "#81A1C1"
